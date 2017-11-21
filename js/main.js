7a45030e7cde5a5da7f0d2f4b4366c3767f52846
craigbrown313 <craigbrown313@gmail.com>
2017-11-20 21:57:11 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+
+	$('#open-left-canvas').click(function(){
+		$('.left-canvas').toggle();
+	});
+
+	$('#close-left-canvas').click(function(){
+		$('.left-canvas').toggle();
+	});
+
+});
